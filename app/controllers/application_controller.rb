c048b4d9aea83fa5c4494015700b2dc3bf2e28ce
DabooUA <sbardysh@tutanota.com>
2022-01-10 15:06:01 -0500

    creating sessions and related parts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception
-  before_action :verified_user
-  helper_method :current_user
-
-  private
-    def verified_user
-      redirect_to '/' unless user_is_authenticated
-    end
-
-    def user_is_authenticated
+  # protect_from_forgery with: :exception
+  # before_action :verified_user
+  # helper_method :current_user
+ 
+    def logged_in?
       !!current_user
     end
 
