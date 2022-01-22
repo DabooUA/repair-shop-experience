4d1d316689a79a69f32ce613df7a72c77afd9118
DabooUA <sbardysh@tutanota.com>
2022-01-22 10:31:08 -0500

    adding comments atrribute to RepairshopSerializer

--- app/serializers/repairshop_serializer.rb
+++ app/serializers/repairshop_serializer.rb
@@ -1,4 +1,13 @@
 class RepairshopSerializer
   include FastJsonapi::ObjectSerializer
   attributes :repair_shop_name, :street_address, :city, :state, :zip_code
+
+  attribute :comments do |shop| 
+    shop.comments.map do |c|
+      {
+        content: c.content
+      }
+    end
+  end
+  # has_many :comments, serializer: CommentSerializer
 end
